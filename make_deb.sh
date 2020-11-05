d6dc7b73913e87ad5e3a082c9fcd7a062f2c136a
Peter Pouliot <peter@pouliot.net>
2020-11-05 16:12:19 -0500

    removing uneeded line

--- make_deb.sh
+++ make_deb.sh
@@ -5,20 +5,13 @@ GRUB_CHERRY_PICK="a6838bbc6726ad624bd2b94991f690b8e9d23c69"
 DEB_BUILD_OPTIONS=nocheck 
 GIT_USER_NAME="Peter Pouliot"
 GIT_USER_EMAIL="ppouliot@amperecomputing.com"
-
 source /ENVIRONMENT
 export $DEBIAN_FRONTEND
 export $DEB_BUILD_OPTIONS
-
 echo "DEB_GRUB_SRC: " ${DEB_GRUB_SRC}
 echo "DEB_GRUB_BRANCH: " ${DEB_GRUB_BRANCH}
-
 mkdir -p /output/${DEB_GRUB_BRANCH}
-
 sed -i -- 's/# deb-src/deb-src/g' /etc/apt/sources.list 
-
-ln -snf /usr/share/zoneinfo/$(curl https://ipapi.co/timezone) /etc/localtime
-
 apt-get update -y
 #apt-get install -y --no-install-recommends git build-essential cdbs devscripts equivs fakeroot flex bison help2man texinfo xfonts-unifont libfreetype6-dev libdevmapper-dev libsdl1.2-dev xorriso parted libfuse-dev fonts-dejavu-core liblzma-dev dosfstools mtools wamerican pkg-config bash-completion libefiboot-dev libefivar-dev python ttf-dejavu-core 
 apt-get install -y --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" ${PACKAGES}
